283d6f4a1960d2cd7daf6099d98c4c28420be0f7
alejandrocalderonhernandez <alejandro950825@gmail.com>
2023-07-24 13:35:46 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { ClientService } from './client.service';
+import { MyType } from './type.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'security-front';
+
+  constructor(private clientService:ClientService){}
+ 
+  response!: string;
+
+  ngOnInit() {
+    this.clientService.callRequest().subscribe(r => this.response = r.msj)
+
+  }
 }
